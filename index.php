a8b78d729210f3a8eaa4d66cf720e7692230488f
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-10 22:16:34 +0300

    5: Laracasts - Configuration Flexibility

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ require 'functions.php';
 require 'Database.php';
 // require 'router.php';
 
+$config = require 'config.php';
 
-$db = new Database();
+$db = new Database($config);
 $posts = $db->query('select * from notes')->fetchAll();
 
 foreach ($posts as $post) {
