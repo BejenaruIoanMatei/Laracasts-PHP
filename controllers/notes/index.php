c1da08ca1e44386e422904954ea4937759cbf1bd
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-13 16:04:02 +0300

    13: Laracasts - Resourceful Naming Conventions

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -7,4 +7,4 @@ $db = new Database($config['database']);
 $heading = 'Notes';
 $notes = $db->query('select * from notes where user_id = 4')->get();
 
-require 'views/notes.view.php';
+require 'views/notes/index.view.php';
