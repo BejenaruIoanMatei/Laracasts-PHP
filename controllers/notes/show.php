650d7c7e9664b61b98f2f7956b08c8d983ae01c3
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-14 13:52:57 +0300

    16: Laracasts - Better Router

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -9,13 +9,31 @@ $db = new Database($config['database']);
 
 $currentUserId = 4;
 
-$note = $db->query('select * from notes where id = :id',[
-    'id' => $_GET['id']
-])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-authorize($note['user_id'] === $currentUserId, Response::FORBIDDEN);
+    $note = $db->query('select * from notes where id = :id',[
+        'id' => $_GET['id']
+    ])->findOrFail();
+    
+    authorize($note['user_id'] === $currentUserId, Response::FORBIDDEN);
 
-view('notes/show.view.php', [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    $db->query('delete from notes where id = :id',[
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+    
+} else {
+
+    $note = $db->query('select * from notes where id = :id',[
+        'id' => $_GET['id']
+    ])->findOrFail();
+    
+    authorize($note['user_id'] === $currentUserId, Response::FORBIDDEN);
+    
+    view('notes/show.view.php', [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
+
