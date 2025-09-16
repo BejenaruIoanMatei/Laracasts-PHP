0ff18cac0fa98e1b2711958c9d7c260ff44814ee
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-16 13:06:56 +0300

    21: Laracasts - Register a New User

---
+++ controllers/registration/store.php
@@ -0,0 +1,51 @@
+<?php
+
+use Core\Validator;
+use Core\App;
+use Core\Database;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+
+if (! Validator::email($email))
+{
+    $errors['email'] = 'Please provide a valid email address.';
+}
+
+if (! Validator::string($password, 7, 10))
+{
+    $errors['password'] = 'Provide a password between 7 and 10 characters.';
+}
+
+if (! empty($errors))
+{
+    return view('registration/create.view.php',[
+        'errors' => $errors
+    ]);
+}
+
+$db = App::resolve(Database::class);
+
+$user = $db->query('select * from users where email = :email',[
+    'email' => $email
+])->find();
+
+if ($user){
+    header('location: /');
+    exit();
+    
+} else {
+    $db->query('insert into users(email, password) values (:email, :password)',[
+        'email' => $email,
+        'password' => $password
+    ]);
+
+    $_SESSION['user'] = [
+        'email' => $email
+    ];
+
+    header('location: /');
+    exit();
+}
